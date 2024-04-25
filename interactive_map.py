38233b69ff1613046ed9e1fdd62834aa75ec64ae
Andrew Truong <165354849+AndrewDTruong@users.noreply.github.com>
2024-04-24 21:13:39 -0700

    updated versions of both these files

--- interactive_map.py
+++ interactive_map.py
@@ -2,25 +2,18 @@ import pandas as pd
 import folium
 import streamlit as st
 from streamlit_folium import folium_static
+from geopy.geocoders import Nominatim
+
 
 #Headers
 st.markdown("# 💧 These are locations of storm drains in Downtown San Jose 💧")
 st.sidebar.markdown("# Page 2: 💧 Stormdrains in Downtown San Jose💧")
 
-
 # Read the location data from the Excel file
 df = pd.read_excel('locations.xlsx')
 
-# Strip leading/trailing spaces from column names
-df.columns = df.columns.str.strip()
-
-# Write the DataFrame to Streamlit to check it
-st.write(df)
-
-st.write("# This is a map of Downtown San Jose")
-
 # Create a text box for street name input
-street_name = st.text_input('Enter a street name:')
+street_name = st.text_input('Enter a stormdrain location based on the chart below:')
 
 # Filter the DataFrame based on the street name
 filtered_df = df[df['Location'].str.contains(street_name, case=False, na=False)]
@@ -28,9 +21,17 @@ filtered_df = df[df['Location'].str.contains(street_name, case=False, na=False)]
 # Display the filtered DataFrame
 st.write(filtered_df)
 
+st.write("# This is a map of Downtown San Jose")
+
 # Create a map centered around the first location in the filtered DataFrame
 if not filtered_df.empty:
-    m = folium.Map(location=[filtered_df.iloc[0]['Latitude'], filtered_df.iloc[0]['Longitude']], zoom_start=10)
+    # If a street name has been entered, use a higher zoom level
+    if street_name:
+        zoom_start = 15
+    else:
+        zoom_start = 10
+
+    m = folium.Map(location=[filtered_df.iloc[0]['Latitude'], filtered_df.iloc[0]['Longitude']], zoom_start=zoom_start)
 
     # Add a marker for each location in the filtered DataFrame
     for index, row in filtered_df.iterrows():
